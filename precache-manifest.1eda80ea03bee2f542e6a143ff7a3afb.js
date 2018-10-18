168d691610ddc7d3a7add2edd9e9687c2953cc25
Phi Xuan Hoan <36753744+hoanphi2201@users.noreply.github.com>
2018-10-18 23:06:37 +0700

    18/10/2018

---
+++ precache-manifest.1eda80ea03bee2f542e6a143ff7a3afb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "15cff150c431d903e72e",
+    "url": "/static/js/main.15cff150.chunk.js"
+  },
+  {
+    "revision": "f76c7cb6ab4f73a27873",
+    "url": "/static/js/1.f76c7cb6.chunk.js"
+  },
+  {
+    "revision": "fada4347e1138a671ea9eccded6389dc",
+    "url": "/index.html"
+  }
+];
